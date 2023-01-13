cb7731f288bedaa557cacd4324ff6d6e281e2b5e
Mickael Dollé <dolle.mickael@gmail.com>
2023-01-13 08:56:17 +0100

    ajout du publicpath

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+const publicPath = process.env.NODE_ENV === "production" ? "/portfolio/" : "/";
+module.exports = {
+  publicPath: publicPath,
+};
